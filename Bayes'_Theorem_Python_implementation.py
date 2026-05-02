f9245b37538b95e7331bebcd7b55db995bfb36c9
Tayler Braga <bragatayler002@gmail.com>
2026-05-02 12:54:38 -0700

    Complete the practice exam coding questions and made sure the code doesn't hog up memory by closing all plots and collecting garbage before plotting the new graph.

--- Bayes'_Theorem_Python_implementation.py
+++ Bayes'_Theorem_Python_implementation.py
@@ -1,18 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import gc
 
+plt.close('all')
+gc.collect()
 truePositiveRate = 0.95
 trueNegativeRate = 0.90
 trueNegAndFalsePos = trueNegativeRate + (1 - truePositiveRate)
 truePosAndFalseNeg = truePositiveRate + (1 - trueNegativeRate)
+steps = 100
 def BayesRule(prevalence, sensitivity, specificity):
     return (prevalence * sensitivity)/ ((prevalence * sensitivity)+((1-prevalence)*(1-specificity)))
 testEffectiveness = []
+xvalues = []
 prevelences = 10**-5
 while prevelences < 0.5:
+    xvalues.append(prevelences)
     testEffectiveness.append(BayesRule(prevelences, truePositiveRate, trueNegativeRate))
-    prevelences *= 10
-plt.plot(prevelences, testEffectiveness)
+    prevelences += (0.5-10**-5)/steps
+plt.plot(xvalues, testEffectiveness)
+plt.axhline(y=0.5, color='r', linestyle='--', label='Prevalence where test is 50/50')
 plt.xscale('log')
 plt.xlabel('Prevalence')
 plt.ylabel('Probability of being sick given a positive test')
